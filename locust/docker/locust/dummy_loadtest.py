3d64268d661c399cff4d8aa1e48c9cb9850a0dae
kuo1 <henry2270@gmail.com>
2019-07-23 11:20:03 +0800

    split yaml file to master-slave

--- locust/docker/locust/dummy_loadtest.py
+++ locust/docker/locust/dummy_loadtest.py
@@ -4,11 +4,18 @@ class DummyTaskSet(TaskSet):
 
     @task(1)
     def find(self):
-        self.client.get("/")
+        header = {'content-type': 'application/json'}
+        payload = {
+            "access_token": "FEF16453FBF28DE0F4CD4597E04E3189F0B769A74A4E0201B6",
+            "nonce_str": "abc123",
+            "sign": "DCD972098347772E52FD70187A43FC88"
+        }
+
+        self.client.post("/findAll", json=payload, headers=header)
 
 
 class DummyLoadTester(HttpLocust):
-    host = "https://stage4-iio.gashpoint.com"
+    host = "https://stage4-iioapi.gashpoint.com/public/api/position"
     task_set = DummyTaskSet
-    min_wait = 5000
-    max_wait = 15000
+    min_wait = 1000
+    max_wait = 5000
